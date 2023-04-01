b8a28734e90b78a45840fcdd32efa7671d1396a8
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2023-04-01 12:09:07 -0700

    Update config.py

--- eduu/config.py
+++ eduu/config.py
@@ -3,9 +3,9 @@ import os.path
 from typing import List, Optional
 
 
-API_ID: int = insert_your_own_api_id_here
-API_HASH: str = "insert_your_own_api_hash_here"
-TOKEN: str = "insert_your_bot_token_here"
+API_ID: int = 2984107
+API_HASH: str = "4ce83ad7d954da391c659c6d7d76e0ce"
+TOKEN: str = "6069510582:AAE9rXONh8q0Zsf0Ixv-Uwl4J7NieALyCLM"
 
 log_chat: int = insert_log_chat_id_here
 sudoers: List[int] = [1757169682, 859229457]
